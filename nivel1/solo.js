93f2698b2e5ce0e2e8fc3814be9f8b52b3390fbd
Filipenobre17 <100380646+Filipenobre17@users.noreply.github.com>
2022-08-02 22:01:19 -0300

    Add files via upload

---
+++ nivel1/solo.js
@@ -0,0 +1,24 @@
+class Solo 
+{
+  constructor(x, y, w,h) 
+  {
+    let options = {
+     isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+
+  mostrar() {
+    let pos = this.body.position;
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill(148,127,146);
+    rect(pos.x,pos.y, this.w, this.h);
+    pop();
+  }
+}
